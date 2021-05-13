116189a07576c6e58271052cc765db812d800c3b
ShreshthaAggarwal155 <71484800+ShreshthaAggarwal155@users.noreply.github.com>
2021-05-13 12:56:13 +0530

    Update bfs.c

--- Lab5/bfs.c
+++ Lab5/bfs.c
@@ -5,10 +5,10 @@ void bfs(int v){
     int i;
 	for (i=1;i<=n;i++)
 	  if(a[v][i] && !visited[i])
-	   q[++r]=i;
+	    q[++r]=i;
 	if(f<=r) {
-		visited[q[f]]=1;
-		bfs(q[f++]);
+	  visited[q[f]]=1;
+	  bfs(q[f++]);
 	}
 }
 int main() {
@@ -17,27 +17,27 @@ int main() {
 	scanf("%d",&n);
 	printf("\nEnter matrix: \n");
 	for (i=1;i<=n;i++){
-      printf("Enter row %d : \n",i);
+          printf("Enter row %d : \n",i);
 	  for (j=1;j<=n;j++)
-	   scanf("%d",&a[i][j]);
+	    scanf("%d",&a[i][j]);
 	}
 	for (i=1;i<=n;i++){
-		q[i]=0;
-		visited[i]=0;
+	  q[i]=0;
+	  visited[i]=0;
 	}
 	printf("\nEnter the beginning vertex : ");
 	scanf("%d",&v);
-    double bfs_time=0.0;
-    clock_t begin=clock();
+        double bfs_time=0.0;
+        clock_t begin=clock();
 	bfs(v);
 	clock_t end=clock();
 	bfs_time+=(double)(end-begin)/CLOCKS_PER_SEC;
 	printf("Visitable Nodes are : \n");
 	for (i=1;i<=n;i++)
 	  if(visited[i])
-	   printf("%d ",i);
-      else
-	   printf("\nBfs is not possible");
-    printf("\nn=%d\tTime:%f\n",n,bfs_time);
+	    printf("%d ",i);
+          else
+	    printf("\nBfs is not possible");
+        printf("\nn=%d\tTime:%f\n",n,bfs_time);
 	return 0;
 }
